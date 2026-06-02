cbf2f581daf91e34b9d3da36ae9ec10bde4c5156
Michal Cholewczynski <niedzwiedz442@proton.me>
2026-06-02 23:59:15 +0200

    added header component and custom fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Bitsumishi",
+      cssVariable: "--font-bitsumishi",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Bitsumishi-Regular.ttf"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter"
+    },
+  ],
+});
