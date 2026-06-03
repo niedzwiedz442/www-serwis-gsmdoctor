2432ad0757078ce9527f55392b942f26177c9fb3
Niedzwiedz442 <michal@cholewczynski.pl>
2026-06-03 11:04:50 +0200

    added custom fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Bitsumishi",
+    cssVariable: "--font-bitsumishi",
+    options: {
+      variants: [{
+        src: ["./src/assets/fonts/Bitsumishi.ttf"],
+        wieght: "normal",
+        style: "normal"
+      }]
+    }
+  },
+  {
+    provider: fontProviders.fontsource(),
+    name: "Inter",
+    cssVariable: "--font-inter",
+  }]
 });
